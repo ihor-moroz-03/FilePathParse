69f59d6cc6c0c72db194a33eb8f73e110c08b35f
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-10-08 23:34:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FilePathParse
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"c:\WebServers\home\testsite\www\myfile.txt";
+            string fileName = Regex.Match(path, @"\w+\.").Value[..^1];
+            string rootFolderName = Regex.Match(path, @":\\\w+").Value[2..];
+
+            Console.WriteLine($"file name: {fileName}");
+            Console.WriteLine($"root folder: {rootFolderName}");
+
+            Console.Read();
+        }
+    }
+}
